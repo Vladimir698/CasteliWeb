41e2a461f0d81c79bca303236fe92ed77571697b
Vladimir698 <145503934+Vladimir698@users.noreply.github.com>
2026-09-18 12:23:33 -0600

    Elimina acceso automatico y protege aplicacion

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const path = require('path');
 const http = require('http');
 const expressLayouts = require('express-ejs-layouts');
+const helmet = require('helmet');
 const { Server } = require('socket.io');
 
 require('dotenv').config();
@@ -36,8 +37,10 @@ app.set('layout extractStyles', true);
 // MIDDLEWARES
 // =============================
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.disable('x-powered-by');
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(express.urlencoded({ extended: true, limit: '100kb' }));
+app.use(express.json({ limit: '100kb' }));
 
 app.set('trust proxy', 1);
 
@@ -130,23 +133,8 @@ const facturacionRoutes = require('./src/routes/facturacionRoutes');
 
 app.use('/', authRoutes);
 
-app.get('/', (req, res) => {
-
-  req.session.usuario = {
-  id: 1,
-  nombre: 'Administrador',
-  apellidos: 'Casteli',
-  usuario: 'admin',
-  correo: 'admin@casteli.com',
-  rol: 'administrador'
-};
-
-  return res.redirect('/home');
-});
-
-app.get('/home', (req, res) => {
-  res.render('index');
-});
+app.get('/', (req,res)=>res.redirect(req.session?.usuario?'/home':'/login'));
+app.get('/home', requiereLogin, (req,res)=>res.render('index'));
 
 
 app.use(
