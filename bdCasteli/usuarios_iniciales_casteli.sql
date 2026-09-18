936b1a4d0f5e20787fe1d4322fe4ab6a8ae968e5
Vladimir698 <145503934+Vladimir698@users.noreply.github.com>
2026-09-18 12:33:52 -0600

    Corrige hashes bcrypt de usuarios iniciales

--- bdCasteli/usuarios_iniciales_casteli.sql
+++ bdCasteli/usuarios_iniciales_casteli.sql
@@ -4,11 +4,11 @@
 
 INSERT INTO usuarios (rol_id,nombre,usuario,password_hash,activo,created_at,updated_at)
 VALUES
-((SELECT id FROM roles WHERE nombre='Mecanico'),'Emerson','emerson','$2b$12$ZM/aEDBsz68veLsgU3jv7.ZyrKn7SzsLtep4FsfUi.jrjk7.Q52uy',TRUE,NOW(),NOW()),
-((SELECT id FROM roles WHERE nombre='Mecanico'),'Anderson','anderson','$2b$12$ZQF2eapc9gnLLYFzHjERhOkjp9fRi3wfMyCqXiAX8JgWSefaJ2ava',TRUE,NOW(),NOW()),
-((SELECT id FROM roles WHERE nombre='Administrador'),'Mariano','mariano','$2b$12$VRBxAmfuaP5Pn8MmussvBeMMrik3cQ/L7MZE37B2qonTP7dg87kQC',TRUE,NOW(),NOW()),
-((SELECT id FROM roles WHERE nombre='Administrador'),'Rodolfo','rodolfo','$2b$12$cmm4K6SAs7bWmtKKiIi7keC.XbGTL4mu9Oo7jVdOzvN8U.SMYgFAO',TRUE,NOW(),NOW()),
-((SELECT id FROM roles WHERE nombre='Administrador'),'Vladimir','vladimir','$2b$12$7OBuTA8Wqvgijzwx3Yxw1e4WVOycj24y8LHuJJnVvVKvJZv.GhSPe',TRUE,NOW(),NOW())
+((SELECT id FROM roles WHERE nombre='Mecanico'),'Emerson','emerson','$2b$12$ocWFNtrE.OT.zc7AMp1r1uw/7Tlpn1y6b54y2aUX8/QxCaJv2c3NG',TRUE,NOW(),NOW()),
+((SELECT id FROM roles WHERE nombre='Mecanico'),'Anderson','anderson','$2b$12$YyfbZgxOLLvOFDiTgGiR3uV4xbpCYcJeJ6vqzE3tQdmlEfj8ShGHW',TRUE,NOW(),NOW()),
+((SELECT id FROM roles WHERE nombre='Administrador'),'Mariano','mariano','$2b$12$qsso1rsQFpqoiKu2QAjkVeCOLziLWQVocZYCCTQHLbYPnZ4SffjrS',TRUE,NOW(),NOW()),
+((SELECT id FROM roles WHERE nombre='Administrador'),'Rodolfo','rodolfo','$2b$12$FUygAQUaZ26svWucOMj8neR/jJpqR2GIYp7wz0mYtQQ8Vgv93QEHe',TRUE,NOW(),NOW()),
+((SELECT id FROM roles WHERE nombre='Administrador'),'Vladimir','vladimir','$2b$12$BmfgGgrrghnG5tznzU/cvO4yzLjECvb/y9HojTA5mS.WSctBFA/Im',TRUE,NOW(),NOW())
 ON CONFLICT (usuario) DO UPDATE SET
  rol_id=EXCLUDED.rol_id,
  nombre=EXCLUDED.nombre,
