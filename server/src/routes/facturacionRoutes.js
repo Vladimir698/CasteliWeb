cf9d2ed151c58ca81929f935e56c14923c82f3db
Vladimir698 <145503934+Vladimir698@users.noreply.github.com>
2026-09-18 12:23:35 -0600

    Restringe facturacion quincenal a administradores

--- server/src/routes/facturacionRoutes.js
+++ server/src/routes/facturacionRoutes.js
@@ -1,9 +1,9 @@
 'use strict';
 const express=require('express');
 const c=require('../controllers/facturacionQuincenalController');
-const {requiereLogin}=require('../middleware/authMiddleware');
+const {requiereLogin,soloAdmin}=require('../middleware/authMiddleware');
 const router=express.Router();
-router.use(requiereLogin);
+router.use(requiereLogin,soloAdmin);
 router.get('/',c.listar);
 router.post('/:id/gti',c.enviarGTI);
 module.exports=router;
