3053942c23171fd4de6ccccbd208b2e76f6f83a3
Vladimir698 <145503934+Vladimir698@users.noreply.github.com>
2026-09-20 14:42:52 -0600

    Retira accion GTI de vista quincenal

--- server/src/routes/facturacionRoutes.js
+++ server/src/routes/facturacionRoutes.js
@@ -5,5 +5,4 @@ const {requiereLogin,soloAdmin}=require('../middleware/authMiddleware');
 const router=express.Router();
 router.use(requiereLogin,soloAdmin);
 router.get('/',c.listar);
-router.post('/:id/gti',c.enviarGTI);
 module.exports=router;
